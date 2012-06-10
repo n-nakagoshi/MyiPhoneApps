98157b1f77780c68d5c54aa57f02edc8c66eddb3
Naruyuki Nakagoshi <n-nakagoshi@ann.hi-ho.ne.jp>
2012-06-10 16:21:26 +0900

    first commit

---
+++ myApps/myApps/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  myApps
+//
+//  Created by 中越 成幸 on 12/06/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
